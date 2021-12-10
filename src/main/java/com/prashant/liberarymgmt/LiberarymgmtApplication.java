a5d8a403cb493fcf9f011007989cb507ca612122
prashantmokani <prashantmokani@outlook.com>
2021-12-10 23:41:12 +0530

    caching enabled and banner changed

--- src/main/java/com/prashant/liberarymgmt/LiberarymgmtApplication.java
+++ src/main/java/com/prashant/liberarymgmt/LiberarymgmtApplication.java
@@ -2,8 +2,10 @@ package com.prashant.liberarymgmt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class LiberarymgmtApplication {
 
     public static void main(String[] args) {
